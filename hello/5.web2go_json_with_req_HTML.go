ae283831bcfd05ac4700533a72217cbacfe83523
Madina <123440519+sleepingforest100@users.noreply.github.com>
2023-12-19 19:05:18 +0600

    feature: added only String input

--- hello/5.web2go_json_with_req_HTML.go
+++ hello/5.web2go_json_with_req_HTML.go
@@ -21,6 +21,27 @@ func sendRequest(w http.ResponseWriter, r *http.Request) {
 	// Вывод полученных данных
 	fmt.Println("Получен запрос:", string(body))
 
+	// Проверка наличия поля "message" в JSON-сообщении
+	var requestData map[string]interface{}
+	err = json.Unmarshal(body, &requestData)
+	if err != nil {
+		http.Error(w, "Некорректное JSON-сообщение", http.StatusBadRequest)
+		return
+	}
+
+	message, exists := requestData["message"]
+	if !exists || message == nil {
+		http.Error(w, "Некорректное JSON-сообщение: отсутствует или имеет некорректное значение поле 'message'", http.StatusBadRequest)
+		return
+	}
+
+	// Проверка типа значения поля "message"
+	_, ok := message.(string)
+	if !ok {
+		http.Error(w, "Некорректное JSON-сообщение: поле 'message' должно быть строкой", http.StatusBadRequest)
+		return
+	}
+
 	// "Отправка" JSON-ответа (в реальности сервер должен отправлять ответ веб-сервису)
 	response := map[string]interface{}{
 		"status":  "success",
