46249cd8e5cc8029206eed283f398f4561982779
Madina <123440519+sleepingforest100@users.noreply.github.com>
2023-12-19 18:23:24 +0600

    init commit

---
+++ hello/5.web2go_json_with_req_HTML.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func sendRequest(w http.ResponseWriter, r *http.Request) {
+	// Добавление заголовка Access-Control-Allow-Origin
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// Чтение данных из тела запроса
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Ошибка чтения тела запроса", http.StatusBadRequest)
+		return
+	}
+
+	// Вывод полученных данных
+	fmt.Println("Получен запрос:", string(body))
+
+	// "Отправка" JSON-ответа (в реальности сервер должен отправлять ответ веб-сервису)
+	response := map[string]interface{}{
+		"status":  "success",
+		"message": "Данные успешно получены",
+	}
+
+	// Преобразование данных в формат JSON
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Ошибка преобразования данных в JSON", http.StatusInternalServerError)
+		return
+	}
+
+	// Запись JSON-ответа в тело HTTP-ответа
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
+}
+
+func main() {
+	// Зарегистрируйте обработчик запросов
+	http.HandleFunc("/", sendRequest)
+
+	// Запуск HTTP-сервера на порту 8080
+	fmt.Println("Сервер запущен на порту 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Ошибка при запуске сервера:", err)
+	}
+}
